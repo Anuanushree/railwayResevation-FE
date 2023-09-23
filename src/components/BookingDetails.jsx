53b15be91ec1811f7bc94dfe780d7156cdbeb9dc
Anuanushree <anuabi2408@gmail.com>
2023-09-23 10:44:29 +0530

    first commit

---
+++ src/components/BookingDetails.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
+
+import { useNavigate } from 'react-router-dom';
+import Navigater from './navigator/nav';
+
+function BookingDetails({ BASE_URL }) {
+    const [chartData, setChartData] = useState([]);
+    const token = localStorage.getItem('token');
+    const headers = {
+        headers: { "authorization": `${token}` }
+    }
+    useEffect(() => {
+        getDetails();
+    }, []);
+    const getDetails = async () => {
+        try {
+            const response = await axios.get(`${BASE_URL}/user/data`, headers);
+            console.log(response.data);
+            setChartData(response.data)
+        } catch (error) {
+            console.log("error in create room :", error);
+
+        }
+    }
+    console.log(chartData)
+    return (
+        <>
+            <Navigater />
+            <div class="container-fluid px-1 py-5 mx-auto">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
+                        <h3>INDIAN RAILWAYS PASSENGER RESERVATION ENQUIRY</h3>
+                        <h4><marquee className=" pt-4" width="80%" direction="left" height="100px">
+                            <marquee class="red-text" direction="left" height="100px">
+                                Please help Indian Railway and government of India in moving towards and cashless economy
+                            </marquee>
+                        </marquee></h4>
+                        <div class="card">
+                            <table className='table bordered table-primary'>
+                                <thead>
+                                    <th>Date</th>
+                                    <th>Train number</th>
+                                    <th>Train name</th>
+                                    <th>From station</th>
+                                    <th>To station</th>
+                                    <th>Timing</th>
+                                    <th>Seat</th>
+                                    <th>Fare</th>
+                                    <th>Payment</th>
+                                </thead>
+                                <tbody>
+                                    {
+                                        chartData.map((data) => (
+                                            <tr key={data._id}>
+                                                <td>{data.date}</td>
+                                                <td>{data.tnumber}</td>
+                                                <td>{data.tname}</td>
+                                                <td>{data.fromStation}</td>
+                                                <td>{data.toStation}</td>
+                                                <td>{data.timing}</td>
+                                                <td>{data.seat}</td>
+                                                <td>{data.price}</td>
+                                                <td>{data.paymentStatus}</td>
+                                            </tr>
+                                        ))
+                                    }
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default BookingDetails
---
+++ src/payment/Payment.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import './payment.css'
+import Navigater from '../components/navigator/nav';
+
+function Payment({BASE_URL}) {
+
+    const [payment, setpayment] = useState('payed');
+    const [error, setError] = useState('')
+
+    const token = localStorage.getItem('token');
+    console.log(token);
+    const bookId = localStorage.getItem('bookId');
+    console.log(bookId);
+    const Trainid = localStorage.getItem('Trainid');
+    const seat = localStorage.getItem('seat');
+
+    const navigate = useNavigate();
+    const handlepay = async (event) => {
+        event.preventDefault();
+        setpayment("payment success")
+        const headers = {
+            headers: { "authorization": `${token}` }
+        }
+
+        try {
+            const create = {
+                payment,
+                bookId,
+                Trainid,
+                seat
+            }
+            const response = await axios.post(`${BASE_URL}/user/payment`, create, headers);
+            console.log(response.data);
+            navigate('/booked')
+          
+
+        } catch (error) {
+            console.log("error in create room :", error);
+            // setError(error.response.data.error);
+
+        }
+    }
+    return (
+        <>
+            <div class="container p-0">
+                <div class="card px-4">
+                    <p class="h8 py-3">Payment Details</p>
+                    <div class="row gx-3">
+                        <div class="col-12">
+                            <div class="d-flex flex-column">
+                                <p class="text mb-1">Person Name</p>
+                                <input class="form-control mb-3" type="text" placeholder="Name" required/>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="d-flex flex-column">
+                                <p class="text mb-1">Card Number</p>
+                                <input class="form-control mb-3" type="text" placeholder="1234 5678 435678" required/>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="d-flex flex-column">
+                                <p class="text mb-1">Expiry</p>
+                                <input class="form-control mb-3" type="text" placeholder="MM/YYYY" required/>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="d-flex flex-column">
+                                <p class="text mb-1">CVV/CVC</p>
+                                <input class="form-control mb-3 pt-2 " type="password" placeholder="***" required/>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="btn btn-primary mb-3">
+                                <span class="ps-3" onClick={handlepay}>Pay </span>
+                                <span class="fas fa-arrow-right"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+                                               
+    )
+}
+
+export default Payment
