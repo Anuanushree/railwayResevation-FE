8c8e3edb0673b9f4e670a26f32afdd9a85cf09ba
Anuanushree <anuabi2408@gmail.com>
2023-09-26 08:33:30 +0530

    completed

--- src/payment/Payment.jsx
+++ src/payment/Payment.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './payment.css'
 import Navigater from '../components/navigator/nav';
 
-function Payment({BASE_URL}) {
+function Payment({ BASE_URL }) {
 
     const [payment, setpayment] = useState('payed');
     const [error, setError] = useState('')
@@ -34,7 +34,7 @@ function Payment({BASE_URL}) {
             const response = await axios.post(`${BASE_URL}/user/payment`, create, headers);
             console.log(response.data);
             navigate('/booked')
-          
+
 
         } catch (error) {
             console.log("error in create room :", error);
@@ -46,43 +46,45 @@ function Payment({BASE_URL}) {
         <>
             <div className="container m-4 payment-body payment-container p-0">
                 <div className="card payment-card px-4">
-                    <p className="h8 py-3">Payment Details</p>
-                    <div className="row gx-3">
-                        <div className="col-12">
-                            <div className="d-flex flex-column">
-                                <p className="text mb-1">Person Name</p>
-                                <input className="form-control mb-3" type="text" placeholder="Name" required/>
+                    <form onSubmit={handlepay}>
+                        <p className="h8 py-3">Payment Details</p>
+                        <div className="row gx-3">
+                            <div className="col-12">
+                                <div className="d-flex flex-column">
+                                    <p className="text mb-1">Person Name</p>
+                                    <input className="form-control mb-3" type="text" placeholder="Name" required />
+                                </div>
+                            </div>
+                            <div className="col-12">
+                                <div className="d-flex flex-column">
+                                    <p className="text mb-1">Card Number</p>
+                                    <input className="form-control mb-3" type="text" placeholder="1234 5678 435678" required />
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="d-flex flex-column">
+                                    <p className="text mb-1">Expiry</p>
+                                    <input className="form-control mb-3" type="text" placeholder="MM/YYYY" required />
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="d-flex flex-column">
+                                    <p className="text mb-1">CVV/CVC</p>
+                                    <input className="form-control mb-3 pt-2 " type="password" placeholder="***" required />
+                                </div>
+                            </div>
+                            <div className="col-12">
+                                <div className="btn btn-primary mb-3">
+                                    <button className="ps-3 btn btn-primary" type='submit'>Pay </button>
+                                    <span className="fas fa-arrow-right"></span>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-12">
-                            <div className="d-flex flex-column">
-                                <p className="text mb-1">Card Number</p>
-                                <input className="form-control mb-3" type="text" placeholder="1234 5678 435678" required/>
-                            </div>
-                        </div>
-                        <div className="col-6">
-                            <div className="d-flex flex-column">
-                                <p className="text mb-1">Expiry</p>
-                                <input className="form-control mb-3" type="text" placeholder="MM/YYYY" required/>
-                            </div>
-                        </div>
-                        <div className="col-6">
-                            <div className="d-flex flex-column">
-                                <p className="text mb-1">CVV/CVC</p>
-                                <input className="form-control mb-3 pt-2 " type="password" placeholder="***" required/>
-                            </div>
-                        </div>
-                        <div className="col-12">
-                            <div className="btn btn-primary mb-3">
-                                <span className="ps-3" onClick={handlepay}>Pay </span>
-                                <span className="fas fa-arrow-right"></span>
-                            </div>
-                        </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </>
-                                               
+
     )
 }
 
